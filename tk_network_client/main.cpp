9920a8b19b4d63a89b124fe702a9099f56f66424
Roma <romanivanov21@mail.ru>
2014-10-25 18:58:51 -0700

    Create cpp and cmake files

---
+++ tk_network_client/main.cpp
@@ -0,0 +1,9 @@
+#include "tk_network_client.h"
+#include "return_tk_network_client.h"
+
+#include <iostream>
+
+int main(void) {
+	
+	return 0;
+}
---
+++ tk_network_client/tk_network_client.h
@@ -0,0 +1,12 @@
+#ifndef _TK_NETWORK_CLIENT_H_
+#define _TK_NETWORK_CLIENT_H_
+
+class network_client
+{
+public:
+	network_client();
+	~network_client();
+private:
+};
+
+#endif
